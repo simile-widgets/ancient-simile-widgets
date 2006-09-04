2ce53e7bc99c0d552e4b9dcbc2f8d0df1c4bc5d3
dfhuynh <dfhuynh@579c892a-5248-0410-a714-2bcf037eb5bf>
2006-09-04 22:05:28 +0000

    Added utility for constructing live-clipboard buttons

--- ajax/trunk/src/webapp/api/scripts/graphics.js
+++ ajax/trunk/src/webapp/api/scripts/graphics.js
@@ -6,6 +6,10 @@
 SimileAjax.Graphics = new Object();
 SimileAjax.Graphics.pngIsTranslucent = (!SimileAjax.Platform.browser.isIE) || (SimileAjax.Platform.browser.majorVersion > 6);
 
+/*==================================================
+ *  Opacity, translucency
+ *==================================================
+ */
 SimileAjax.Graphics.createTranslucentImage = function(doc, url, verticalAlign) {
     var elmt;
     if (SimileAjax.Graphics.pngIsTranslucent) {
@@ -31,6 +35,11 @@ SimileAjax.Graphics.setOpacity = function(elmt, opacity) {
         elmt.style.MozOpacity = o;
     }
 };
+
+/*==================================================
+ *  Animation
+ *==================================================
+ */
 
 SimileAjax.Graphics.createAnimation = function(f, from, to, duration) {
     return new SimileAjax.Graphics._Animation(f, from, to, duration);
@@ -71,4 +80,48 @@ SimileAjax.Graphics._Animation.prototype.step = function() {
     } else {
         this.f(this.to, 0);
     }
+};
+
+/*==================================================
+ *  CopyPasteButton
+ *
+ *  Adapted from http://spaces.live.com/editorial/rayozzie/demo/liveclip/liveclipsample/techPreview.html.
+ *==================================================
+ */
+
+SimileAjax.Graphics.createStructuredDataCopyButton = function(image, width, height, createDataFunction) {
+    var div = document.createElement("div");
+    div.style.position = "relative";
+    div.style.display = "inline";
+    div.style.width = width + "px";
+    div.style.height = height + "px";
+    div.style.overflow = "hidden";
+    div.style.margin = "2px";
+    
+    if (SimileAjax.Graphics.pngIsTranslucent) {
+        div.style.background = "url(" + image + ") no-repeat";
+    } else {
+        div.style.filter = "progid:DXImageTransform.Microsoft.AlphaImageLoader(src='" + image +"', sizingMethod='image')";
+    }
+    
+    var style;
+    if (SimileAjax.Platform.browser.isIE) {
+        style = "filter:alpha(opacity=0)";
+    } else {
+        style = "opacity: 0";
+    }
+    div.innerHTML = "<textarea rows='1' autocomplete='off' value='none' style='" + style + "' />";
+    
+    var textarea = div.firstChild;
+    textarea.style.width = width + "px";
+    textarea.style.height = height + "px";
+    textarea.onmousedown = function(evt) {
+        evt = (evt) ? evt : ((event) ? event : null);
+        if (evt.button == 2) {
+            textarea.value = createDataFunction();
+            textarea.select();
+        }
+    };
+    
+    return div;
 };
