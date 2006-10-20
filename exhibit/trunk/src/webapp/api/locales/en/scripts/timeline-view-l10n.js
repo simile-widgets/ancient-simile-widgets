37b32399901490d87cd21328d0fe350b65714eb1
dfhuynh <dfhuynh@579c892a-5248-0410-a714-2bcf037eb5bf>
2006-10-20 10:25:37 +0000

    Added timeline view. Forgot a file.

---
+++ exhibit/trunk/src/webapp/api/locales/en/scripts/timeline-view-l10n.js
@@ -0,0 +1,14 @@
+/*==================================================
+ *  Exhibit.TimelineView English localization
+ *==================================================
+ */
+ 
+Exhibit.TimelineView.l10n = {
+    viewLabel:          "Timeline",
+    viewTooltip:        "View items on a timeline",
+    mixedLegendKey:     "Mixed",
+    colorLegendTitle:   "Color Legend",
+    formatMappableCount: function(count) {
+        return "Only " + count + " can be plotted on the timeline.";
+    }
+};
