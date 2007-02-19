2fb471569960ef4b8584235bc545fb00860d04ff
dfhuynh <dfhuynh@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-02-19 17:38:25 +0000

    Added HTML exporter for grabbing generated HTML code.

--- exhibit/tags/1.0/src/webapp/api/exhibit-api.js
+++ exhibit/tags/1.0/src/webapp/api/exhibit-api.js
@@ -46,6 +46,7 @@
             "exporters/semantic-wikitext-exporter.js",
             "exporters/exhibit-json-exporter.js",
             "exporters/tsv-exporter.js",
+            "exporters/html-exporter.js",
             "exporters/bibtex-exporter.js"
         ];
         var cssFiles = [
