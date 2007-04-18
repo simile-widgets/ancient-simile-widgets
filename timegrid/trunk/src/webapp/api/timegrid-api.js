8646208bec1e96c64a4f3db0897f1ad4b5dfabb3
masont <masont@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-04-18 18:07:56 +0000

    Added some basic framework code from Timeline

---
+++ timegrid/trunk/src/webapp/api/timegrid-api.js
@@ -0,0 +1,9 @@
+/* ===========================================================================
+ * Timegrid API
+ *     This file will load all the necessary Javascript files to make a 
+ * standard Timegrid operate.
+ * ===========================================================================
+ */
+
+ var Timegrid = new Object();
+ Timegrid.Platform = new Object();
