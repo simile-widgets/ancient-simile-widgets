495c617e490720101d3752c4bce18c91feefc14a
oyasumi <oyasumi@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-05-06 12:40:19 +0000

    I was a fair bit into copying this to a new view to get rid of the table (which is much, much more difficult to work with in the CSS domain) before realizing I'd be better off just replacing the painstakingly emulated <ol>+<li> tag combination in TileView with the real thing. Getting rid of the number prefixes is now done via ol.exhibit-tileView-body { margin:0; padding:0; } plus ol.exhibit-tileView-body li { list-style-type:none; }

--- exhibit/branches/2.0/src/webapp/api/scripts/ui/views/tile-view.js
+++ exhibit/branches/2.0/src/webapp/api/scripts/ui/views/tile-view.js
@@ -6,14 +6,14 @@
 Exhibit.TileView = function(containerElmt, uiContext) {
     this._div = containerElmt;
     this._uiContext = uiContext;
-    
+
     var view = this;
-    
+
     this._listener = { onItemsChanged: function() { view._reconstruct(); } };
     uiContext.getCollection().addListener(this._listener);
-    
+
     this._orderedViewFrame = new Exhibit.OrderedViewFrame(uiContext);
-    this._orderedViewFrame.parentReconstruct = function() { view._reconstruct(); }
+    this._orderedViewFrame.parentReconstruct = function() { view._reconstruct(); };
 };
 
 Exhibit.TileView.create = function(configuration, containerElmt, uiContext) {
@@ -22,7 +22,7 @@ Exhibit.TileView.create = function(configuration, containerElmt, uiContext) {
         Exhibit.UIContext.create(configuration, uiContext)
     );
     view._orderedViewFrame.configure(configuration);
-    
+
     view._initializeUI();
     return view;
 };
@@ -30,27 +30,27 @@ Exhibit.TileView.create = function(configuration, containerElmt, uiContext) {
 Exhibit.TileView.createFromDOM = function(configElmt, containerElmt, uiContext) {
     var configuration = Exhibit.getConfigurationFromDOM(configElmt);
     var view = new Exhibit.TileView(
-        containerElmt != null ? containerElmt : configElmt, 
+        containerElmt != null ? containerElmt : configElmt,
         Exhibit.UIContext.createFromDOM(configElmt, uiContext)
     );
-    
+
     view._orderedViewFrame.configureFromDOM(configElmt);
     view._orderedViewFrame.configure(configuration);
-    
+
     view._initializeUI();
     return view;
 };
 
 Exhibit.TileView.prototype.dispose = function() {
     this._uiContext.getCollection().removeListener(this._listener);
-    
+
     this._div.innerHTML = "";
-    
+
     this._toolboxWidget.dispose();
     this._orderedViewFrame.dispose();
     this._orderedViewFrame = null;
     this._dom = null;
-    
+
     this._div = null;
     this._uiContext = null;
 };
@@ -74,7 +74,7 @@ Exhibit.TileView.prototype._initializeUI = function() {
     };
     this._dom = SimileAjax.DOM.createDOMFromTemplate(template);
     this._toolboxWidget = Exhibit.ToolboxWidget.createFromDOM(this._div, this._div, this._uiContext);
-    
+
     var self = this;
     this._orderedViewFrame._divHeader = this._dom.headerDiv;
     this._orderedViewFrame._divFooter = this._dom.footerDiv;
@@ -82,7 +82,7 @@ Exhibit.TileView.prototype._initializeUI = function() {
         return self._dom.bodyDiv;
     };
     this._orderedViewFrame.initializeUI();
-        
+
     this._reconstruct();
 };
 
@@ -90,81 +90,69 @@ Exhibit.TileView.prototype._reconstruct = function() {
     var view = this;
     var state = {
         div:            this._dom.bodyDiv,
-        table:          null,
+        contents:       null,
         groupDoms:      [],
         groupCounts:    []
     };
-    
+
     var closeGroups = function(groupLevel) {
         for (var i = groupLevel; i < state.groupDoms.length; i++) {
             state.groupDoms[i].countSpan.innerHTML = state.groupCounts[i];
         }
         state.groupDoms = state.groupDoms.slice(0, groupLevel);
         state.groupCounts = state.groupCounts.slice(0, groupLevel);
-        
+
         if (groupLevel > 0) {
             state.div = state.groupDoms[groupLevel - 1].contentDiv;
         } else {
             state.div = view._dom.bodyDiv;
         }
-        state.table = null;
+        state.contents = null;
     }
-    
+
     this._orderedViewFrame.onNewGroup = function(groupSortKey, keyType, groupLevel) {
         closeGroups(groupLevel);
-        
+
         var groupDom = Exhibit.TileView.constructGroup(groupLevel, groupSortKey);
-        
+
         state.div.appendChild(groupDom.elmt);
         state.div = groupDom.contentDiv;
-        
+
         state.groupDoms.push(groupDom);
         state.groupCounts.push(0);
     };
-    
+
     this._orderedViewFrame.onNewItem = function(itemID, index) {
-        //if (index > 10) return;
-        
-        if (state.table == null) {
-            state.table = Exhibit.TileView.constructTable();
-            state.div.appendChild(state.table);
+        if (state.contents == null) {
+            state.contents = Exhibit.TileView.constructList();
+            state.div.appendChild(state.contents);
         }
-        
+
         for (var i = 0; i < state.groupCounts.length; i++) {
             state.groupCounts[i]++;
         }
-        
-        var rows = state.table.rows;
-        var tr = state.table.insertRow(rows.length);
-        
-        var tdIndex = tr.insertCell(0);
-        tdIndex.className = "exhibit-tileView-itemIndex";
-        tdIndex.innerHTML = (index + 1) + ".";
-        
-        var tdItemLens = tr.insertCell(1);
-        
-        var itemLensDiv = document.createElement("div");
-        var itemLens = view._uiContext.getLensRegistry().createLens(itemID, itemLensDiv, view._uiContext);
-        tdItemLens.appendChild(itemLensDiv);
+
+        var itemLensItem = document.createElement("li");
+        var itemLens = view._uiContext.getLensRegistry().createLens(itemID, itemLensItem, view._uiContext);
+        state.contents.appendChild( itemLensItem );
     };
-                
+
     this._div.style.display = "none";
-    
+
     this._dom.bodyDiv.innerHTML = "";
     this._orderedViewFrame.reconstruct();
     closeGroups(0);
-    
+
     this._div.style.display = "block";
 };
 
 Exhibit.TileView.constructGroup = function(groupLevel, label) {
-    var l10n = Exhibit.TileView.l10n;
     var template = {
         tag: "div",
         className: "exhibit-collectionView-group",
         children: [
             {   tag: "h" + (groupLevel + 1),
-                children: [ 
+                children: [
                     label,
                     {   tag:        "span",
                         className:  "exhibit-collectionView-group-count",
@@ -188,9 +176,8 @@ Exhibit.TileView.constructGroup = function(groupLevel, label) {
     return SimileAjax.DOM.createDOMFromTemplate(template);
 };
 
-Exhibit.TileView.constructTable = function() {
-    var table = document.createElement("table");
-    table.className = "exhibit-tileView-body";
-    return table;
+Exhibit.TileView.constructList = function() {
+    var div = document.createElement("ol");
+    div.className = "exhibit-tileView-body";
+    return div;
 };
-    
