3f79c195f273cbb733f10e525402bd81466e8403
masont <masont@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-06-21 20:50:53 +0000

    No longer require class="timegrid" on Timegrid elements, added progress-running.gif

--- timegrid/trunk/src/webapp/api/scripts/create.js
+++ timegrid/trunk/src/webapp/api/scripts/create.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
-    $('div.timegrid').each(function() {
-        if (!window.timegrids) { window.timegrids = new Array(); }
-        window.timegrids.push(Timegrid.createFromDOM(this));
+    $('div').each(function() {
+        if ($(this).attrs('tg').role == "grid") {
+            if (!window.timegrids) { window.timegrids = new Array(); }
+            window.timegrids.push(Timegrid.createFromDOM(this));
+        }
     });
 });
