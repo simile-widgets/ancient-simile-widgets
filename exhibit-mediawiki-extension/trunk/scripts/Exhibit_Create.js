b6db37988bd048754d25236a37a072070f3c0198
leibovic <leibovic@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-07-13 17:48:45 +0000

    Added code for accepting data and column variables generated in Exhibit_Main.php

--- exhibit-mediawiki-extension/trunk/scripts/Exhibit_Create.js
+++ exhibit-mediawiki-extension/trunk/scripts/Exhibit_Create.js
@@ -3,49 +3,45 @@
  */
 
 function createExhibit() {
-	
-	if ($('div.exhibit').attr('ex:data')) {
-	
-		var dataTable = document.getElementById($('div.exhibit').attr('ex:data'));
-		var columns = ($('div.exhibit').attr('ex:columns')).split(',');	
-		var th, ths = dataTable.getElementsByTagName("th");
-	    for( col = 0; th = ths[col]; col++ ) {
-			var label = columns[col];
-			th.setAttribute('ex:name', label);
-		}
 
-		var viewClass = $('div.view').attr('viewClass');	
-		var lens = $('div.lens').get();	
-		var facets = [];
-	
-		window.database = Exhibit.Database.create();
-		Exhibit.HtmlTableImporter.loadTable(dataTable, window.database); 
-		window.exhibit = Exhibit.create(window.database);
-		
-		var exhibitDiv = document.createElement('div');
-		exhibitDiv.innerHTML = "<table width='100%'><tr valign='top'><td><div id='view'></div></div></td><td width='20%' id='facets'></td></tr></table>";
-		dataTable.parentNode.insertBefore(exhibitDiv, dataTable);
-		
-		var configurationComponents = [
-			{   elmt:    document.getElementById("view"),
-	  			role:    "view"
-			}
-		];
-		for (index in facets) {
-			if (facets[index]) {
-				var degreeFacetDiv = document.createElement("div");
-				document.getElementById("facets").appendChild(degreeFacetDiv);
-				configurationComponents.push(
-					{   elmt:        degreeFacetDiv,
-						role:        "facet",
-						expression:  "." + index
-					});
-			}
-		};
-		window.exhibit.configure({
-			components: configurationComponents
-		});
+	var dataTable = document.getElementById(data);	
+	var th, ths = dataTable.getElementsByTagName("th");
+	for( col = 0; th = ths[col]; col++ ) {
+		var label = columns[col];
+		th.setAttribute('ex:name', label);
 	}
+
+	var viewClass = $('div.view').attr('viewClass');	
+	var lens = $('div.lens').get();	
+	var facets = [];
+
+	window.database = Exhibit.Database.create();
+	Exhibit.HtmlTableImporter.loadTable(dataTable, window.database); 
+	window.exhibit = Exhibit.create(window.database);
+	
+	var exhibitDiv = document.createElement('div');
+	exhibitDiv.innerHTML = "<table width='100%'><tr valign='top'><td><div id='view'></div></div></td><td width='20%' id='facets'></td></tr></table>";
+	dataTable.parentNode.insertBefore(exhibitDiv, dataTable);
+	
+	var configurationComponents = [
+		{   elmt:    document.getElementById("view"),
+			role:    "view"
+		}
+	];
+	for (index in facets) {
+		if (facets[index]) {
+			var degreeFacetDiv = document.createElement("div");
+			document.getElementById("facets").appendChild(degreeFacetDiv);
+			configurationComponents.push(
+				{   elmt:        degreeFacetDiv,
+					role:        "facet",
+					expression:  "." + index
+				});
+		}
+	};
+	window.exhibit.configure({
+		components: configurationComponents
+	});
 }
 
 addOnloadHook(createExhibit);
