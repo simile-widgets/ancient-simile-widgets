6c8625f9ee02eaf40c810eba601cf9773af0ed24
stefano <stefano@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-07-16 23:15:06 +0000

    finishing and fixing the tutorial

--- timeplot/trunk/src/webapp/site/docs/create-timeplots.js
+++ timeplot/trunk/src/webapp/site/docs/create-timeplots.js
@@ -33,35 +33,113 @@ function onLoad() {
           dataSource: new Timeplot.ColumnSource(eventSource,1),
           valueGeometry: new Timeplot.DefaultValueGeometry({
           	gridColor: new Timeplot.Color("#000000"),
+          	axisLabelsPlacement: "left",
             max: 100,
             min: 0
           })
        })
     ];
-
-    var red = new Timeplot.Color("#880000");
-
+    
     var plotInfo4 = [
        Timeplot.createPlotInfo({
           id: "plot1",
           dataSource: new Timeplot.ColumnSource(eventSource,1),
           valueGeometry: new Timeplot.DefaultValueGeometry({
-            gridColor: new Timeplot.Color("#000000"),
+            gridColor: "#000000",
+            axisLabelsPlacement: "left",
             max: 100,
             min: 0
           }),
-          lineColor: red,
-          fillColor: red.lighten(150)
+          lineColor: "#ff0000",
+          fillColor: "#cc8080",
        })
     ];
-                
+
+    var plotInfo5 = [
+       Timeplot.createPlotInfo({
+          id: "plot1",
+          dataSource: new Timeplot.ColumnSource(eventSource,1),
+          valueGeometry: new Timeplot.DefaultValueGeometry({
+            gridColor: "#000000",
+            axisLabelsPlacement: "left",
+            max: 100,
+            min: 0
+          }),
+          lineColor: "#ff0000",
+          fillColor: "#cc8080",
+          showValues: true
+       })
+    ];
+
+    var timeGeometry1 = new Timeplot.DefaultTimeGeometry();
+
+    var valueGeometry = new Timeplot.DefaultValueGeometry({
+        gridColor: "#000000",
+        min: 0,
+        max: 100
+    });
+      
+    var plotInfo6 = [
+	    Timeplot.createPlotInfo({
+		  id: "plot1",
+		  dataSource: new Timeplot.ColumnSource(eventSource,1),
+		  timeGeometry: timeGeometry1,
+          valueGeometry: valueGeometry,
+		  lineColor: "#ff0000",
+		  fillColor: "#cc8080",
+		  showValues: true
+		}),
+		Timeplot.createPlotInfo({
+		  id: "plot2",
+		  dataSource: new Timeplot.ColumnSource(eventSource,3),
+          timeGeometry: timeGeometry1,
+          valueGeometry: valueGeometry,
+		  lineColor: "#D0A825",
+		  showValues: true
+		})
+    ];
+
+    var eventSource2 = new Timeplot.DefaultEventSource();
+
+    var timeGeometry2 = new Timeplot.DefaultTimeGeometry();
+
+    var plotInfo7 = [
+        Timeplot.createPlotInfo({
+          id: "plot1",
+          dataSource: new Timeplot.ColumnSource(eventSource,1),
+          timeGeometry: timeGeometry2,
+          valueGeometry: valueGeometry,
+          lineColor: "#ff0000",
+          fillColor: "#cc8080",
+          showValues: true
+        }),
+        Timeplot.createPlotInfo({
+          id: "plot2",
+          dataSource: new Timeplot.ColumnSource(eventSource,3),
+          timeGeometry: timeGeometry2,
+          valueGeometry: valueGeometry,
+          lineColor: "#D0A825",
+          showValues: true
+        }),
+        Timeplot.createPlotInfo({
+          id: "plot3",
+          timeGeometry: timeGeometry2,
+          eventSource: eventSource2,
+          lineColor: "#85DB18",
+        })
+    ];
+                  
     timeplots[0] = Timeplot.create(document.getElementById("timeplot0"), plotInfo0);
     timeplots[1] = Timeplot.create(document.getElementById("timeplot1"), plotInfo1);
     timeplots[2] = Timeplot.create(document.getElementById("timeplot2"), plotInfo2);
     timeplots[3] = Timeplot.create(document.getElementById("timeplot3"), plotInfo3);
     timeplots[4] = Timeplot.create(document.getElementById("timeplot4"), plotInfo4);
+    timeplots[5] = Timeplot.create(document.getElementById("timeplot5"), plotInfo5);
+    timeplots[6] = Timeplot.create(document.getElementById("timeplot6"), plotInfo6);
+    timeplots[7] = Timeplot.create(document.getElementById("timeplot7"), plotInfo7);
 
     timeplots[1].loadText("../examples/bush/bush_ratings.txt", ",", eventSource);
+    timeplots[1].loadXML("../examples/bush/bush_events.xml", eventSource2);
 }
 
 var resizeTimerID = null;
