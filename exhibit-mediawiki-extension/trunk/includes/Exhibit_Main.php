dd2d165e5215d3c9250584b093d74f300fe92d1c
losvedir <losvedir@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-07-20 19:22:31 +0000

    Mismatched XML tags no longer kill the page and make it uneditable. Still ugly, though.

--- exhibit-mediawiki-extension/trunk/includes/Exhibit_Main.php
+++ exhibit-mediawiki-extension/trunk/includes/Exhibit_Main.php
@@ -60,55 +60,56 @@ function Exhibit_getHTMLResult( $input, $argv ) {
 
 	// use SimpleXML parser
 	$xmlstr = "<?xml version='1.0' standalone='yes'?><root>$input</root>"; 
-	$xml = new SimpleXMLElement($xmlstr);
-	
-	// <data>
-	$sourceData = array();
-	$sourceColumns = array();
-	$sourceHideTable = array();
-	foreach ($xml->source as $source) {
-		array_push($sourceData, $source);
-		array_push($sourceColumns, $source['columns']);
-		array_push($sourceHideTable, $source['hideTable']);
-	}	
-	$sourceData = implode(',', $sourceData);
-	$sourceColumns = implode(';', $sourceColumns);
-	$sourceHideTable = implode(',', $sourceHideTable);
-	
-	// <configuration>	
-	$facets = array();
-	foreach ($xml->facet as $facet) {
-		$attributes = array();
-		foreach ($facet->attributes() as $a => $b) {
-			$attr = $a."='".$b."'";
-    		array_push( $attributes, $attr);
-    	}
-    	array_push( $facets, implode(',', $attributes));
-	}
-	$facets = implode(';', $facets);
-	
-	$views = array();
-	foreach ($xml->view as $view) {
-		$attributes = array();
-		foreach ($view->attributes() as $a => $b) {
-			$attr = $a."='".$b."'";
-    		array_push( $attributes, $attr);
-    	}
-    	array_push( $views, implode(';', $attributes));
-	}
-	$views = implode('/', $views);
+	try {
+		$xml = new SimpleXMLElement($xmlstr);
+		
+		// <data>
+		$sourceData = array();
+		$sourceColumns = array();
+		$sourceHideTable = array();
+		foreach ($xml->source as $source) {
+			array_push($sourceData, $source);
+			array_push($sourceColumns, $source['columns']);
+			array_push($sourceHideTable, $source['hideTable']);
+		}	
+		$sourceData = implode(',', $sourceData);
+		$sourceColumns = implode(';', $sourceColumns);
+		$sourceHideTable = implode(',', $sourceHideTable);
+		
+		// <configuration>	
+		$facets = array();
+		foreach ($xml->facet as $facet) {
+			$attributes = array();
+			foreach ($facet->attributes() as $a => $b) {
+				$attr = $a."='".$b."'";
+			array_push( $attributes, $attr);
+		}
+		array_push( $facets, implode(',', $attributes));
+		}
+		$facets = implode(';', $facets);
+		
+		$views = array();
+		foreach ($xml->view as $view) {
+			$attributes = array();
+			foreach ($view->attributes() as $a => $b) {
+				$attr = $a."='".$b."'";
+			array_push( $attributes, $attr);
+		}
+		array_push( $views, implode(';', $attributes));
+		}
+		$views = implode('/', $views);
 
-	$output = <<<OUTPUT
-	<script type="text/javascript">
-	var sourceData = "$sourceData".split(',');
-	var sourceColumns = "$sourceColumns".split(';');
-	var sourceHideTable = "$sourceHideTable".split(',');
-	var facets = "$facets".split(';');
-	var views = "$views".split('/');
-	</script>
-	<div id="exhibitLocation"></div>
+		$output = <<<OUTPUT
+		<script type="text/javascript">
+		var sourceData = "$sourceData".split(',');
+		var sourceColumns = "$sourceColumns".split(';');
+		var sourceHideTable = "$sourceHideTable".split(',');
+		var facets = "$facets".split(';');
+		var views = "$views".split('/');
+		</script>
+		<div id="exhibitLocation"></div>
 OUTPUT;
-	
+	} catch( Exception $e) { $output = "<p>Something is wrong with the exhibit tags, sorry.</p>"; }	
 	return $output;
 }
 
