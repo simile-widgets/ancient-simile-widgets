9f4aefe60a1a3946a2d44b76e3f21830143b5591
masont <masont@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-08-14 18:54:16 +0000

    Added property layout example, recurring event source

---
+++ timegrid/trunk/src/webapp/api/scripts/sources/weekly.js
@@ -0,0 +1,123 @@
+/**
+ * @name Timegrid.RecurringEventSource
+ * @author masont
+ */
+ 
+/**
+ * A type of EventSource that allows the creation and display of recurring 
+ * events that are not tied to a specific date, e.g. 8am on MWF.
+ *
+ * @constructor
+ */
+Timegrid.RecurringEventSource = function() {
+    Timegrid.RecurringEventSource.superclass.call(this);
+    
+    /* 
+     * The actual array containing event prototypes is kept private, and only
+     * accessed/modified through priviledged methods created here, in the
+     * constructor. 
+     */
+    var eventPrototypes = new DStructs.Array();
+    
+    //========================= Privileged Methods ==========================//
+    
+    /** Adds the given event prototype to this event source */
+    this.addEventPrototype = function(eventPrototype) {
+        eventPrototypes.push(eventPrototype);
+    };
+    
+    /** Removes the given event prototype from this source's prototypes */
+    this.removeEventPrototype = function(eventPrototype) {
+        return eventPrototypes.remove(eventPrototype);
+    };
+    
+    /** Removes all of the event prototypes from this source */
+    this.clearEventPrototypes = function() {
+        eventPrototypes.clear();
+    };
+
+    /** Generates events from event prototypes */
+    this.generateEvents = function(startDate, endDate) {
+        return eventPrototypes.foldr(new DStructs.Array(), function(a, ep) {
+            a.concat(ep.generateEvents(startDate, endDate));
+            return a;
+        });
+    };
+};
+$.inherit(Timegrid.RecurringEventSource, Timegrid.ListenerAware);
+
+Timegrid.RecurringEventSource.prototype = {
+    loadXML: function(xml, url) {
+    
+    },
+    loadJSON: function(data, url) {
+    
+    },
+    getEventIterator: function(startDate, endDate) {
+        return this.generateEvents(startDate, endDate).iterator();
+    },
+    getEarliestDate: function() {
+    
+    },
+    getLatestDate: function() {
+    
+    }
+};
+
+Timegrid.RecurringEventSource.EventPrototype = function(dayArray, start, end, 
+        text, description, image, link, icon, color, textColor) {
+    var id = "e" + Math.floor(Math.random() * 1000000);
+    var days = new DStructs.Array(); days.addAll(dayArray);
+
+    this.getDays = function() { return days; };
+    this.getStart = function() { return start; };
+    this.getEnd = function() { return end; };
+    
+    this.getID = function() { return id; }
+    this.getText = function() { 
+        return SimileAjax.HTML.deEntify(text); 
+    };
+    this.getDescription = function() { 
+        return SimileAjax.HTML.deEntify(description); 
+    };
+    this.getImage = function() { 
+        return (image != null && image != "") ? image : null;
+    };
+    this.getLink = function() {
+        return (link != null && link != "") ? link : null;
+    };
+    this.getIcon = function() { 
+        return (icon != null && icon != "") ? icon : null;
+    };
+    this.getColor = function() {
+        return (color != null && color != "") ? color : null;
+    };
+    this.getTextColor = function() {
+        return (textColor != null && textColor != "") ? textColor : null;
+    }
+    this.generateFrom = function(date) {
+        if (!this.getDays().contains(date.getDay())) { return false; }
+        var startTime = new Date(this.getStart());
+        var endTime = new Date(this.getEnd());
+        startTime.setDate(date.getDate());
+        startTime.setMonth(date.getMonth());
+        startTime.setFullYear(date.getFullYear());
+        endTime.setDate(date.getDate());
+        endTime.setMonth(date.getMonth());
+        endTime.setFullYear(date.getFullYear());
+        return new Timegrid.DefaultEventSource.Event(startTime, endTime, null,
+                null, false, text, description, image, link, icon, color,
+                textColor);
+    };
+};
+
+Timegrid.RecurringEventSource.EventPrototype.prototype = {
+    generateEvents: function(start, end) {
+        var events = new DStructs.Array();
+        for (var date = new Date(start); date < end; date.add('d', 1)) {
+            var event = this.generateFrom(date);
+            if (event) { events.push(event); }
+        }
+        return events;
+    }
+};
---
+++ timegrid/trunk/src/webapp/site/examples/property/lectures.js
@@ -0,0 +1,29 @@
+{
+  'dateTimeFormat': 'iso8601',
+  'events' : [
+  {
+    'start' : '2007-08-09T09:00',
+    'end' : '2007-08-09T10:00',
+    'title' : '18.01 - Single-Variable Calculus',
+    'room' : '10-250'
+  },
+  {
+    'start' : '2007-08-09T10:00',
+    'end' : '2007-08-09T11:00',
+    'title' : '3.091 - Solid-State Chemistry',
+    'room' : '10-250'
+  },
+  {
+    'start' : '2007-08-09T15:00',
+    'end' : '2007-08-09T17:00',
+    'title' : '8.01 - Physics I',
+    'room' : '26-132'
+  },
+  {
+    'start' : '2007-08-09T15:00',
+    'end' : '2007-08-09T17:00',
+    'title' : '8.02 - Physics II',
+    'room' : '32-052'
+  }
+  ]
+}
