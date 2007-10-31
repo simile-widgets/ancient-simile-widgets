879752e56c3338912e441e94fb62ecfbd692c341
stefano <stefano@579c892a-5248-0410-a714-2bcf037eb5bf>
2007-10-31 20:13:07 +0000

    updating to the latest jquery

--- ajax/trunk/src/webapp/api/simile-ajax-api.js
+++ ajax/trunk/src/webapp/api/simile-ajax-api.js
@@ -166,7 +166,7 @@ if (typeof SimileAjax == "undefined") {
 
     (function() {
         var javascriptFiles = [
-            "jquery-1.1.3.1.js",
+            "jquery-1.2.1.js",
             "platform.js",
             "debug.js",
             "xmlhttp.js",
