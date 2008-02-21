a63b3b179536093d38054a8da355e446735e0345
dfhuynh <dfhuynh@579c892a-5248-0410-a714-2bcf037eb5bf>
2008-02-21 16:23:11 +0000

    Fixed minor copy and paste bug in chart-locale.js.

--- exhibit/trunk/src/webapp/extensions/chart/locales/en/chart-locale.js
+++ exhibit/trunk/src/webapp/extensions/chart/locales/en/chart-locale.js
@@ -13,5 +13,5 @@ if (!("l10n" in Exhibit.PivotTableView)) {
 Exhibit.ScatterPlotView.l10n.viewLabel = "Scatter Plot";
 Exhibit.ScatterPlotView.l10n.viewTooltip = "View items on a scatter plot";
 
-Exhibit.PivotTableView.l10n.viewLabel = "Scatter Plot";
+Exhibit.PivotTableView.l10n.viewLabel = "Pivot Table";
 Exhibit.PivotTableView.l10n.viewTooltip = "View items on a pivot table";
