cc8e49c72597c852bb6b32fb7610a7699d824cd8
eob@mit.edu <eob@mit.edu@579c892a-5248-0410-a714-2bcf037eb5bf>
2008-09-23 13:41:15 +0000

    AJAX: updated to JQuery 1.2.6 - Changing bundles and api reference to new version of jQuery

--- ajax/trunk/src/webapp/api/simile-ajax-api.js
+++ ajax/trunk/src/webapp/api/simile-ajax-api.js
@@ -166,7 +166,7 @@ if (typeof SimileAjax == "undefined") {
 
     (function() {
         var javascriptFiles = [
-            "jquery-1.2.3.js",
+            "jquery-1.2.6.js",
             "platform.js",
             "debug.js",
             "xmlhttp.js",
