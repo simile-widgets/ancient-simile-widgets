75c17009d3fd21e39ba99ea92743896587e7e5c1
dfhuynh <dfhuynh@579c892a-5248-0410-a714-2bcf037eb5bf>
2009-04-02 19:42:47 +0000

    DATADUST Added energy example.

---
+++ datadust/trunk/src/webapp/examples/energy/index.js
@@ -0,0 +1,51 @@
+var widget;
+
+function onLoad() {
+    widget = Datadust.createOrShowInstaller(
+        document.getElementById("the-widget"),
+        {
+            sources: [
+                {   url:        "us-oil-import.txt",
+                    format:     "tab",
+                    hasColumnHeadings: false,
+                    columns: [
+                        { name : "date", type: "string" },
+                        { name : "oil", type: "number" }
+                    ]
+                },
+                {   url:        "gasoline-price.txt",
+                    format:     "tab",
+                    hasColumnHeadings: false,
+                    columns: [
+                        { name : "date", type: "string" },
+                        { name : "gas", type: "number" }
+                    ],
+                    join: "date"
+                }
+            ],
+            prepare: [
+                //{ operation: "sort", expression: "data.year[2000].population", ascending: false }
+            ],
+            configs: [
+                {
+                    tooltip: { expression: "data.date" },
+                    xAxis: {
+                        expression: "data.oil",
+                        scale: "linear"
+                    },
+                    yAxis: {
+                        expression: "data.gas",
+                        scale: "linear"
+                    },
+                    nodes: {
+                        //size: { expression: "power(data.literacy, 2)" }
+                    }
+                }
+            ]
+        }
+    );
+}
+
+function select(index) {
+    widget.selectConfiguration(index);
+}
