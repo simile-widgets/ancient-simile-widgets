1a4f640d666297d9dbdf5f8b4d4e14ac69157dda
dfhuynh <dfhuynh@579c892a-5248-0410-a714-2bcf037eb5bf>
2009-05-17 20:10:24 +0000

    AJAX Switched to using the same loading callback for all APIs so we can have a uniform way of loading a sequence of APIs. Added utility loader.js.

--- ajax/trunk/src/webapp/api/compilations/simile-ajax-compilation.js
+++ ajax/trunk/src/webapp/api/compilations/simile-ajax-compilation.js
@@ -2966,9 +2966,9 @@ SimileAjax.XmlHttp._forceXML=function(C){try{C.overrideMimeType("text/xml");
 
 /* compile-epilog.js */
 (function(){var f=null;
-if(typeof SimileAjax_onLoad=="string"){f=eval(SimileAjax_onLoad);
-SimileAjax_onLoad=null;
-}else{if(typeof SimileAjax_onLoad=="function"){f=SimileAjax_onLoad;
-SimileAjax_onLoad=null;
-}}if(f!=null){f();
+if("SimileWidgets_onLoad" in window){if(typeof SimileWidgets_onLoad=="string"){f=eval(SimileWidgets_onLoad);
+SimileWidgets_onLoad=null;
+}else{if(typeof SimileWidgets_onLoad=="function"){f=SimileWidgets_onLoad;
+SimileWidgets_onLoad=null;
+}}}if(f!=null){f();
 }})();
