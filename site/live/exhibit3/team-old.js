fa2e7b826c52f863cb12cf2f39635feca9fdf5a7
ryanlee@csail.mit.edu <ryanlee@csail.mit.edu@579c892a-5248-0410-a714-2bcf037eb5bf>
2011-01-17 21:56:45 +0000

    adding exhibit3 project section

---
+++ site/live/exhibit3/team-old.js
@@ -0,0 +1,150 @@
+{
+	"items" :  	[
+		{
+			"photo" :     "http://www.mit.edu/~kenzie/mackenzie-smith-100x100.jpg",
+			"url" :       "http://www.mit.edu/~kenzie/",
+			"last-name" : "Smith",
+			"position" :  "MIT Libraries Research Director",
+			"email" :     "kenzie@mit.edu",
+			"type" :      "PI",
+			"label" :     "MacKenzie Smith",
+			"group" :     "PI"
+					
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Eric-Miller-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/eric-miller/",
+			"last-name" : "Miller",
+			"position" :  "Zepheira President",
+			"email" :     "em@zepheira.com",
+			"type" :      "co-PI",
+			"label" :     "Eric Miller",
+			"group" :     "co-PI"
+		},
+		{
+			"photo" :     "http://www.csail.mit.edu/biographies/PI/72dpi/karger.jpg",
+			"url" :       "http://people.csail.mit.edu/karger",
+			"last-name" : "Karger",
+			"position" :  "MIT faculty",
+			"email" :     "karger@csail.mit.edu",
+			"type" :      "co-PI",
+			"label" :     "David Karger",
+			"group" :     [
+					"co-PI",
+					"researcher"
+			]
+
+		},
+		{
+			"photo" :     "Adam.jpg",
+			"url" :       "http://people.csail.mit.edu/marcua",
+			"last-name" : "Marcus",
+			"position" :  "MIT grad student",
+			"email" :     "marcua@csail.mit.edu",
+			"type" :      "student",
+			"label" :     "Adam Marcus",
+			"group" :     [
+				"researcher",
+				"developer"
+			]
+
+		},
+		{
+			"photo" :     "",
+			"url" :       "",
+			"last-name" : "Rodgers",
+			"position" :  "MIT Libraries Head of Software Development and Analysis",
+			"email" :     "rrodgers@mit.edu",
+			"type" :      "Software Architect",
+			"label" :     "Richard Rodgers",
+			"group" :     [
+				"project management",
+				"architect"
+			]
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Uche-Ogbuji-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/uche-ogbuji/",
+			"last-name" : "Ojbuji",
+			"position" :  "Zepheira Partner",
+			"email" :     "uche@zepheira.com",
+			"type" :      "Software Architect",
+			"label" :     "Uche Ogbuji",
+			"group" :     "architect"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Kathy-MacDougall-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/kathy-macdougall/",
+			"last-name" : "MacDougall",
+			"position" :  "Zepheira Partner",
+			"email" :     "kathy@zepheira.com",
+			"type" :      "Project Manager",
+			"label" :     "Kathy MacDougall",
+			"group" :     "project management"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Ryan-Lee-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/ryan-lee/",
+			"last-name" : "Lee",
+			"position" :  "Zepheira Developer",
+			"email" :     "ryan@zepheira.com",
+			"type" :      "Software Developer",
+			"label" :     "Ryan Lee",
+			"group" :     "developer"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/David-Feeney-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/david-feeney/",
+			"last-name" : "Feeney",
+			"position" :  "Zepheira Developer",
+			"email" :     "david@zepheira.com",
+			"type" :      "Software Developer",
+			"label" :     "David Feeney",
+			"group" :     "developer"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Simon-J-Hernandez-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/simon-j-hernandez/",
+			"last-name" : "Hernandez",
+			"position" :  "Zepheira Systems Administrator",
+			"email" :     "simon@zepheira.com",
+			"type" :      "Systems Administrator",
+			"label" :     "Simon Hernandez",
+			"group" :     "sysadmin"
+		},
+		{
+			"photo" :     "http://img.freebase.com/api/trans/image_thumb/m/04kqmmm?errorid=%2Ffreebase%2Fno_image_png&maxheight=110&mode=fit&maxwidth=110",
+			"url" :       "http://davidhuynh.net/",
+			"last-name" : "Huynh",
+			"position" :  "Google Research Scientist",
+			"email" :     "dfhuynh@alum.mit.edu",
+			"type" :      "Software Architect",
+			"label" :     "David Huynh",
+			"group" :     [
+					"architect",
+					"advisor"
+			]
+		},
+		{
+			"photo" :     "http://img.freebase.com/api/trans/image_thumb/m/047k4yj?errorid=%2Ffreebase%2Fno_image_png&maxheight=110&mode=fit&maxwidth=110",
+			"url" :       "http://www.betaversion.org/~stefano/",
+			"last-name" : "Mazzocchi",
+			"position" :  "Google Research Scientist",
+			"email" :     "stefanom@google.com",
+			"type" :      "Software Architect",
+			"label" :     "Stefano Mazzocchi",
+			"group" :     [
+					"architect",
+					"advisor"
+			]
+		}
+	],
+    properties: {
+        url: {
+            valueType: "url"
+        },
+        photo: {
+            valueType: "url"
+        }
+    }
+}
---
+++ site/live/exhibit3/team.js
@@ -0,0 +1,199 @@
+{
+	"items" :  	[
+		{
+			"photo" :     "http://www.mit.edu/~kenzie/mackenzie-smith-100x100.jpg",
+			"url" :       "http://www.mit.edu/~kenzie/",
+			"last-name" : "Smith",
+			"position" :  "Research Director",
+			"email" :     "kenzie@mit.edu",
+			"type" :      "Member",
+			"label" :     "MacKenzie Smith",
+			"role" :     "PI",
+			"organization" : "MIT Libraries"
+					
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Eric-Miller-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/eric-miller/",
+			"last-name" : "Miller",
+			"position" :  "President",
+			"email" :     "em@zepheira.com",
+			"type" :      "Member",
+			"label" :     "Eric Miller",
+			"role" :     "co-PI",
+			"organization" : "Zepheira"
+
+		},
+		{
+			"photo" :     "http://www.csail.mit.edu/biographies/PI/72dpi/karger.jpg",
+			"url" :       "http://people.csail.mit.edu/karger",
+			"last-name" : "Karger",
+			"position" :  "Professor",
+			"email" :     "karger@csail.mit.edu",
+			"type" :      "Member",
+			"label" :     "David Karger",
+			"role" :     [
+					"co-PI",
+					"researcher"
+			],
+			"organization" : "MIT CSAIL"
+
+
+		},
+		{
+			"photo" :     "Adam.jpg",
+			"url" :       "http://people.csail.mit.edu/marcua",
+			"last-name" : "Marcus",
+			"position" :  "Graduate Student",
+			"email" :     "marcua@csail.mit.edu",
+			"type" :      "Member",
+			"label" :     "Adam Marcus",
+			"role" :     [
+				"researcher",
+				"developer"
+			],
+			"organization" : "MIT CSAIL"
+		},
+		{
+			"photo" :     "http://people.csail.mit.edu/eob/images/edward_benson.jpg",
+			"url" :       "http://edwardbenson.com/",
+			"last-name" : "Benson",
+			"position" :  "Graduate Student",
+			"email" :     "eob@csail.mit.edu",
+			"type" :      "Member",
+			"label" :     "Edward Benson",
+			"role" :     [
+				"researcher",
+				"developer"
+			],
+			"organization" : "MIT CSAIL"
+		},
+		{
+			"photo" :     "richard-rodgers-110x110.jpg",
+			"url" :       "http://www.linkedin.com/pub/richard-rodgers/4/147/b2a",
+			"last-name" : "Rodgers",
+			"position" :  "Head of Software Development and Analysis",
+			"email" :     "rrodgers@mit.edu",
+			"type" :      "Member",
+			"label" :     "Richard Rodgers",
+			"role" :     [
+				"project manager",
+				"architect"
+			],
+			"organization" : "MIT Libraries"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Uche-Ogbuji-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/uche-ogbuji/",
+			"last-name" : "Ojbuji",
+			"position" :  "Partner",
+			"email" :     "uche@zepheira.com",
+			"type" :      "Member",
+			"label" :     "Uche Ogbuji",
+			"role" :     "architect",
+			"organization" : "Zepheira"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Kathy-MacDougall-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/kathy-macdougall/",
+			"last-name" : "MacDougall",
+			"position" :  "Partner",
+			"email" :     "kathy@zepheira.com",
+			"type" :      "Member",
+			"label" :     "Kathy MacDougall",
+			"role" :     "project manager",
+			"organization" : "Zepheira"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Ryan-Lee-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/ryan-lee/",
+			"last-name" : "Lee",
+			"position" :  "Developer",
+			"email" :     "ryan@zepheira.com",
+			"type" :      "Member",
+			"label" :     "Ryan Lee",
+			"role" :     "developer",
+			"organization" : "Zepheira"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/David-Feeney-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/david-feeney/",
+			"last-name" : "Feeney",
+			"position" :  "Developer",
+			"email" :     "david@zepheira.com",
+			"type" :      "Member",
+			"label" :     "David Feeney",
+			"role" :     "developer",
+			"organization" : "Zepheira"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Simon-J-Hernandez-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/simon-j-hernandez/",
+			"last-name" : "Hernandez",
+			"position" :  "Systems Administrator",
+			"email" :     "simon@zepheira.com",
+			"type" :      "Member",
+			"label" :     "Simon Hernandez",
+			"role" :     "sysadmin",
+			"organization" : "Zepheira"
+		},
+		{
+			"photo" :     "http://zepheira.com/wp-content/uploads/2010/06/Mark-Baker-110x110.jpg",
+			"url" :       "http://zepheira.com/about/people/mark-baker/",
+			"last-name" : "Baker",
+			"position" :  "Developer",
+			"email" :     "mark@zepheira.com",
+			"type" :      "Member",
+			"label" :     "Mark Baker",
+			"role" :     "developer",
+			"organization" : "Zepheira"
+		},
+		{
+			"photo" :     "http://img.freebase.com/api/trans/image_thumb/m/04kqmmm?errorid=%2Ffreebase%2Fno_image_png&maxheight=110&mode=fit&maxwidth=110",
+			"url" :       "http://davidhuynh.net/",
+			"last-name" : "Huynh",
+			"position" :  "Research Scientist",
+			"email" :     "dfhuynh@alum.mit.edu",
+			"type" :      "Member",
+			"label" :     "David Huynh",
+			"role" :     [
+					"architect",
+					"advisor"
+			],
+			"organization" : "Google"
+		},
+		{
+			"photo" :     "http://img.freebase.com/api/trans/image_thumb/m/047k4yj?errorid=%2Ffreebase%2Fno_image_png&maxheight=110&mode=fit&maxwidth=110",
+			"url" :       "http://www.betaversion.org/~stefano/",
+			"last-name" : "Mazzocchi",
+			"position" :  "Research Scientist",
+			"email" :     "stefanom@google.com",
+			"type" :      "Member",
+			"label" :     "Stefano Mazzocchi",
+			"role" :     [
+					"architect",
+					"advisor"
+			],
+			"organization" : "Google"
+		}
+	],
+    properties: {
+        url: {
+            valueType: "url"
+        },
+        photo: {
+            valueType: "url"
+        },
+	label: {
+	    label: "Name"
+	},
+	"last-name": {
+	    label: "Last Name"
+	}
+    },
+    types: {
+	Member: {
+	    pluralLabel: "Members",
+	}
+    }	
+}
