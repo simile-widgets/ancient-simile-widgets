d6b7ec02928e756dbfd59c79b2823505455d1cba
david.karger <david.karger@579c892a-5248-0410-a714-2bcf037eb5bf>
2011-12-27 19:57:52 +0000

    Handle IE \r\n newlines, and use a simpler parser.

--- exhibit/trunk/src/webapp/api/scripts/data/importers/tsv-csv-importer.js
+++ exhibit/trunk/src/webapp/api/scripts/data/importers/tsv-csv-importer.js
@@ -58,6 +58,8 @@ Exhibit.TsvCsvImporter.parse = function(content, link, url, separator) {
 	    //if properties specified in link, assume no column titles
 	    //unless overridden by specifying hasColumnTitle=true
 	    hasColumnTitles = Exhibit.getAttribute(link, "hasColumnTitles");
+	    if (hasColumnTitles)
+		hasColumnTitles = (hasColumnTitles.toLowerCase() == "true");
 	}
     }
     var valueSeparator=Exhibit.getAttribute(link,"valueSeparator");
@@ -113,85 +115,65 @@ Exhibit.TsvCsvImporter._parseInternal = function(text, separator, expressionStri
 
 Exhibit.TsvCsvImporter.CsvToArray =function(text,separator){
     var i;
-    text = text.replace(/\r\n/g,"\n"); //handle IE newline convention
     if (text.indexOf('"') < 0) { 
 	//fast case: no quotes
-	var lines=text.split('\n');
+	var lines=text.split(/\r?\n/);
+	var items;
 	for (i=0; i<lines.length; i++) {
-	    lines[i]=lines[i].split(separator);
+	    if (lines[i].length > 0)
+		output.push(lines[i].split(separator));
 	} 
-	return lines;
+	return output;
     }
 
     /*end of early return from simple case.  
-      below we have to handle strings with doublequotes
-      doublequotes alternately activate and deactivate quoting
-      paired doublequotes (which appear inside quoted strings to signify a doublequote)
-      can be thought of as doing the same since they cancel
-      paired doublequotes that are _unquoted_ are just quoted empty strings, should vanish
-      while paired doublequotes that are _quoted_ should turn into one doublequote
+      below we have to handle strings with quotemarks
+      single quotes alternately activate and deactivate quoting
+      inside a quoted string, 
+         commas and newlines aren't special
+         a single quote is escaped as two quotes
 
       we want to split lines on newlines that are not "inside" quotes.  
-      and fields on commas that are not "inside" quotes
       in other words, with an even number of quotes preceding
 
-      it would be natural to use "split" here, but split swallows the whole regexp
-      so we can't say "preceded by even quotes" without swallowing the quotes.
-
-      this regexp will work correctly for all valid csv (balanced double quotes)
-      it will return unpredictable values for invalid cases 
-         (odd numbers of quotes, or pairs of doublequotes outside doublequoted fields)
-      a csv line is a newline-terminated sequence of "fragments", each of which is either
-         a string with no double quotes or newlines, or
-         a (possibly empty) string without double quotes but between a pair of double quotes
-      we require at least one fragment to avoid empty lines
       we use a lookahead pattern at the end to avoid including the newline in the match
-      and use nongreedy matching so we find ALL unquoted newlines*/
-
-    var lines=text.match(/(([^\n"]+?)|("[^"]*?"))+?(?=\n|$)/g);
-
-    /*it would be nice to use the same trick for splitting fields on the separator e.g. comma
-      unfortunately empty strings are meaningful in csv: 
-         a,b,,d,e means an empty field between b and d
-      if we modify the above regexp to allow empty strings, we get bitten by lookahead:
-      after matching up to a comma, the regexp next matches the empty string before the comma
-      producing lots of spurious matches
-      it's also hard to distinguish a comma following a real exp and one following a comma
-      also the regexp would need to specifically handle commas at beginning and end of line.
-      big mess.   
-      so we take a different approach
+      and use nongreedy matching so we find ALL unquoted newlines
     */
+    text = text.replace(/\r?\n/g,"\n"); //handle IE newline convention
+    var lines=text.match(/([^"\n]|("[^"]*"))+?(?=\r?\n|$)/g);
+
+    /*it would be nice to use the same trick for splitting fields
+      unfortunately empty strings are meaningful field values in csv
+      if we modify the above regexp to allow empty strings, 
+         javascript regexp matching becomes complicated
+	 http://www.regexguru.com/2008/04/watch-out-for-zero-length-matches/
+      so we take a different approach
+
+      \uFFFF is a "private use character" that will not appear in parsed text
+      http://en.wikipedia.org/wiki/Mapping_of_Unicode_characters#Private_use_characters
+    */
+    
     var records=[];
     for (i=0; i<lines.length; i++) {
-	var pieces=lines[i].split('"');
-	var fields=pieces[0].split(separator); 
-	var j=1;
-	while (j<pieces.length) {
-	    //given fields[last] contains previous unquoted section
-	    //collect next quoted section
-	    //until start of start next unquoted section
-	    //then concatenate quoted sec into last bit of previous unquoted
-	    //and first bit of following unquoted
-
-	    //begin at quoted segment
-	    var quoted=[pieces[j++]];
-	    while (pieces[j].length==0) {
-		//next quote immediately followed by a quote
-		//i.e. found an escaped (doubled) quote
-		j=j+1; //skip over empty---will insert single " here at join 
-		quoted.push(pieces[j++]); //stuff up to next quote is still quoted
-	    }
-	    //nonzero next piece, so found isolated closing quote
-	    quoted=quoted.join('"');
-	    //now in unquoted state
-	    //rfc woiuld say a separator appears next
-	    //meaning fieldsNext[0] is empty string
-	    //but be robust in case there's more string
-	    var fieldsNext=pieces[j++].split(separator);
-	    fieldsNext[0]=fields.pop().concat(quoted,fieldsNext[0]);
-	    [].push.apply(fields,fieldsNext);
+	if (lines[i].length > 0) {
+	    records.push(lines[i]
+			 //prepare to split at separator:
+			 .replace(new RegExp(separator,"g"),'\uFFFF')
+			 //but clean up quoted regions:
+			 .replace(/"([^"]*"")*[^"]*"/g,
+				  function(quoted) {
+				      return quoted
+				      //trim outer quotes
+					  .slice(1,-1)
+				      //bring back original quoted separator
+					  .replace(/\uFFFF/g,separator)
+				      //unescape quotes
+					  .replace(/""/,'"')
+				  })
+			 .split('\uFFFF')
+			);
 	}
-	records.push(fields);
     }
 return records;
 }
+
